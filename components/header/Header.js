ef428a779de788f634329fd6c5d9178ef3a7b6fc
Py-Ashraful <ashrafulislam180@gmail.com>
2022-03-13 20:04:10 +0600

    header

---
+++ components/header/Header.js
@@ -0,0 +1,52 @@
+import Navbar from "react-bootstrap/Navbar";
+import { Nav } from "react-bootstrap";
+import styles from "./header.module.css";
+import { BsFillPersonFill } from "react-icons/bs";
+
+export default function Header() {
+  return (
+    <div>
+      <i className="bi bi-list mobile-nav-toggle d-xl-none"></i>
+      <header
+        id={styles.header}
+        className="d-flex flex-column justify-content-center"
+      >
+        <Navbar id="navbar" className={styles.nav_menu}>
+          <ul>
+            <li>
+              <Nav.Link href="#hero" className="nav-link scrollto active">
+                <BsFillPersonFill className={styles.icons} />
+                <span>Home</span>
+              </Nav.Link>
+            </li>
+            <li>
+              <a href="#about" className="nav-link scrollto">
+                <i className="bx bx-user"></i> <span>About</span>
+              </a>
+            </li>
+            <li>
+              <a href="#resume" className="nav-link scrollto">
+                <i className="bx bx-file-blank"></i> <span>Resume</span>
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="nav-link scrollto">
+                <i className="bx bx-book-content"></i> <span>Portfolio</span>
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="nav-link scrollto">
+                <i className="bx bx-server"></i> <span>Services</span>
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="nav-link scrollto">
+                <i className="bx bx-envelope"></i> <span>Contact</span>
+              </a>
+            </li>
+          </ul>
+        </Navbar>
+      </header>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Button from "react-bootstrap/Button";
-import Navbar from "react-bootstrap/Navbar";
-import { Nav } from "react-bootstrap";
-
+import Header from "../components/header/Header";
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -14,42 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header id="header" className="d-flex flex-column justify-content-center">
-        <Navbar id="navbar" className={styles.nav_menu}>
-          <ul>
-            <li>
-              <Nav.Link href="#hero" className="nav-link scrollto active">
-                <i className="bx bx-home"></i> <span>Home</span>
-              </Nav.Link>
-            </li>
-            <li>
-              <a href="#about" className="nav-link scrollto">
-                <i className="bx bx-user"></i> <span>About</span>
-              </a>
-            </li>
-            <li>
-              <a href="#resume" className="nav-link scrollto">
-                <i className="bx bx-file-blank"></i> <span>Resume</span>
-              </a>
-            </li>
-            <li>
-              <a href="#portfolio" className="nav-link scrollto">
-                <i className="bx bx-book-content"></i> <span>Portfolio</span>
-              </a>
-            </li>
-            <li>
-              <a href="#services" className="nav-link scrollto">
-                <i className="bx bx-server"></i> <span>Services</span>
-              </a>
-            </li>
-            <li>
-              <a href="#contact" className="nav-link scrollto">
-                <i className="bx bx-envelope"></i> <span>Contact</span>
-              </a>
-            </li>
-          </ul>
-        </Navbar>
-      </header>
+      <Header />
 
       <section id="hero" className="d-flex flex-column justify-content-center">
         <div className="container" data-aos="zoom-in" data-aos-delay="100">
@@ -80,6 +41,90 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section id="about" className="about">
+        <div className="container" data-aos="fade-up">
+          <div className="section-title">
+            <h2>About</h2>
+            <p>
+              Magnam dolores commodi suscipit. Necessitatibus eius consequatur
+              ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
+              quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
+              Quia fugiat sit in iste officiis commodi quidem hic quas.
+            </p>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-4">
+              <img
+                src="../public/images/profile-img.jpg"
+                className="img-fluid"
+                alt=""
+              />
+            </div>
+            <div className="col-lg-8 pt-4 pt-lg-0 content">
+              <h3>UI/UX Designer &amp; Web Developer.</h3>
+              <p className="fst-italic">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <div className="row">
+                <div className="col-lg-6">
+                  <ul>
+                    <li>
+                      <i className="bi bi-chevron-right"></i>{" "}
+                      <strong>Birthday:</strong> <span>1 May 1995</span>
+                    </li>
+                    <li>
+                      <i className="bi bi-chevron-right"></i>{" "}
+                      <strong>Website:</strong> <span>www.example.com</span>
+                    </li>
+                    <li>
+                      <i className="bi bi-chevron-right"></i>{" "}
+                      <strong>Phone:</strong> <span>+123 456 7890</span>
+                    </li>
+                    <li>
+                      <i className="bi bi-chevron-right"></i>{" "}
+                      <strong>City:</strong> <span>New York, USA</span>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-lg-6">
+                  <ul>
+                    <li>
+                      <i className="bi bi-chevron-right"></i>{" "}
+                      <strong>Age:</strong> <span>30</span>
+                    </li>
+                    <li>
+                      <i className="bi bi-chevron-right"></i>{" "}
+                      <strong>Degree:</strong> <span>Master</span>
+                    </li>
+                    <li>
+                      <i className="bi bi-chevron-right"></i>{" "}
+                      <strong>PhEmailone:</strong>{" "}
+                      <span>email@example.com</span>
+                    </li>
+                    <li>
+                      <i className="bi bi-chevron-right"></i>{" "}
+                      <strong>Freelance:</strong> <span>Available</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <p>
+                Officiis eligendi itaque labore et dolorum mollitia officiis
+                optio vero. Quisquam sunt adipisci omnis et ut. Nulla
+                accusantium dolor incidunt officia tempore. Et eius omnis.
+                Cupiditate ut dicta maxime officiis quidem quia. Sed et
+                consectetur qui quia repellendus itaque neque. Aliquid amet
+                quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis
+                culpa magni laudantium dolores.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* <!-- End About Section --> */}
     </div>
   );
 }
