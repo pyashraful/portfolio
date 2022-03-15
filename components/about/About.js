324196b2a26dbb9a90800ffb44ef7b03fa15e6dd
Py-Ashraful <ashrafulislam180@gmail.com>
2022-03-15 18:39:11 +0600

    resume and skill section

--- components/about/About.js
+++ components/about/About.js
@@ -35,51 +35,7 @@ export default function About() {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
-            {/* <div className="row">
-              <div className="col-lg-6">
-                <ul>
-                  <li>
-                    <i className="bi bi-chevron-right"></i>{" "}
-                    <strong>Birthday:</strong> <span>1 May 1995</span>
-                  </li>
-                  <li>
-                    <i className="bi bi-chevron-right"></i>{" "}
-                    <strong>Website:</strong> <span>www.example.com</span>
-                  </li>
-                  <li>
-                    <i className="bi bi-chevron-right"></i>{" "}
-                    <strong>Phone:</strong> <span>+123 456 7890</span>
-                  </li>
-                  <li>
-                    <i className="bi bi-chevron-right"></i>{" "}
-                    <strong>City:</strong> <span>New York, USA</span>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-lg-6">
-                <ul>
-                  <li>
-                    <i className="bi bi-chevron-right"></i>{" "}
-                    <strong>Age:</strong> <span>30</span>
-                  </li>
-                  <li>
-                    <i className="bi bi-chevron-right"></i>{" "}
-                    <strong>Degree:</strong> <span>Master</span>
-                  </li>
-                  <li>
-                    <i className="bi bi-chevron-right"></i>{" "}
-                    <strong>PhEmailone:</strong> <span>email@example.com</span>
-                  </li>
-                  <li>
-                    <i className="bi bi-chevron-right"></i>{" "}
-                    <strong>Freelance:</strong> <span>Available</span>
-                  </li>
-                </ul>
-              </div>
-            </div> */}
-
             <Details />
-
             <p>
               Officiis eligendi itaque labore et dolorum mollitia officiis optio
               vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor
